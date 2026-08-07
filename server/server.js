3294ad0fb8b105177043103dc226d60e00460568
Dhruvi Kalariya <dhruvikalariya011@gmail.com>
2026-08-08 00:39:52 +0530

    added 5173 in CORS

--- server/server.js
+++ server/server.js
@@ -10,7 +10,10 @@ import { protect } from "./middlewares/authmiddleware.js";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 app.use(clerkMiddleware())
 
 
