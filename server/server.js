6e82196eb8c0ef1360cf13b2cb2c29d8f37fbf10
Dhruvi Kalariya <dhruvikalariya011@gmail.com>
2026-08-08 15:50:12 +0530

    created api for project

--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ import { serve } from "inngest/express";
 import { inngest, functions } from "./inngest/index.js"
 import workspaceRouter from "./routes/workspaceRoutes.js";
 import { protect } from "./middlewares/authmiddleware.js";
+import projectRouter from "./routes/projectRoutes.js";
 
 const app = express();
 
@@ -25,6 +26,8 @@ app.use("/api/inngest", serve({ client: inngest, functions }));
 
 //Routes
 app.use('/api/workspaces',protect, workspaceRouter)
+app.use('/api/projects', protect, projectRouter)
+
 
 const PORT = process.env.PORT || 4000;
 
